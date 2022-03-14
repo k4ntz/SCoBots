1ebc1b0258d88bb494834f1c6e38a6c82850f69d
Dwarak Adugodi Vittal <dwarakvittal@MacBook-Pro-von-Dwarak-2.local>
2022-03-14 10:56:34 +0100

    only import necessary env

--- src/xrl/environments/env_manager.py
+++ src/xrl/environments/env_manager.py
@@ -1,14 +1,13 @@
 # file to load the correct env
 
-import xrl.environments.agym as agym
-import xrl.environments.pgym as pgym
-
 # notify: Print creations
 def make(cfg, notify=False):
     env_name = cfg.env_name
     if notify:
         print("Env Name:", env_name)
     if "coin" in env_name:
+        import xrl.environments.pgym as pgym
         return pgym.make(env_name, notify)
     else:
+        import xrl.environments.agym as agym
         return agym.make(env_name, notify)
