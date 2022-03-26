375e9cae3d64c8016f19c9d0bf70ecd6c93d52b4
sebastian <605763+sebawastaken@users.noreply.github.com>
2022-03-26 19:48:10 +0100

    bash script typo

--- src/run_pong_jobs.sh
+++ src/run_pong_jobs.sh
@@ -3,7 +3,7 @@ echo "Starting Jobs.."
 for i in 1 2 3
 do
     python train.py --config ../configs/seb-pong-no-feedback_$i.yaml > xrl/relogs/no_feedback_$i.log &
-    python train.py --config ../configs/seb-pong-feedback_$i.yaml > xrl/relogsfeedback_$i.log &
+    python train.py --config ../configs/seb-pong-feedback_$i.yaml > xrl/relogs/feedback_$i.log &
     sleep 2
 done
 wait
