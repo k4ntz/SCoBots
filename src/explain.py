ae561ec76bd5405af94af3bac8a748392f04df27
Dwarak Adugodi Vittal <dwarakvittal@mbpvondwarak2.speedport.ip>
2022-05-23 20:13:29 +0200

    first try decision tree as approximation

---
+++ src/explain.py
@@ -0,0 +1,7 @@
+from xrl_start import xrl
+from xrl.utils.utils import get_config
+from xrl.genetic_rl import policy_net
+
+if __name__ == '__main__':
+    cfg = get_config()
+    xrl(cfg, "explain")
