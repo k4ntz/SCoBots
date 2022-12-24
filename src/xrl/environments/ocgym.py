77167f68bb2297c92765d9e1431abf82c8adc960
sebastian <605763+sebawastaken@users.noreply.github.com>
2022-12-24 05:15:23 +0100

    merge resolved

--- src/xrl/environments/ocgym.py
+++ src/xrl/environments/ocgym.py
@@ -5,11 +5,7 @@ import gym
 from termcolor import colored
 try:
     from ocatari.core import OCAtari
-<<<<<<< HEAD
 except ImportError as imp_err:
-=======
-except ModuleNotFoundError as imp_err:
->>>>>>> focus file support first version
     print(colored("OC-Atari Not found, please install it:", "red"))
     print(colored("https://github.com/k4ntz/OC_Atari", "blue"))
     #print("test")
