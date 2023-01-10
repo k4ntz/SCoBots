3fa9094d0ecbecfdded9cef9eb3fd9877ef6b8fb
Sebastian Szt <szt.sebastian@googlemail.com>
2023-01-10 22:34:24 +0100

    genetic eval working, refactoring done?

--- README.md
+++ README.md
@@ -1,54 +1,36 @@
-# XmodRL
-Explainable Modular Reinforcement Learning (from images)
+# scobi
+Successive Concepet Bottleneck Interface
+
+
+TODO: SeSz REWORK ME
 
 ## What is happening here?
+you need ocatari
 
-TODO: Insert image of task splitting
 
-This repository contains a modular reinforcement learning framework. Instead of using one algorithm for the whole reinforcement learning task, this framework gives the possibility to choose specific solver for subtasks of a reinforcement learning problem. The three subtasks are the following:
-
-1. Extracting raw features
-2. Processing them to meaningful features
-3. Generating a policy from meaninful features
+raw -> object bottleneck -> concept bottleneck -> env
 
 ## How to simple call
 
-Pretrained models and their config files are available inside ```pretrained/```. To use one of the pretrained models:
+
 
 ```
-sh setup_pretrained.sh
-cd src
-
-# for just evaluating a trained model
-python eval.py --config ../pretrained/configs/pretrained-boxing-gen.yaml
-
 # for training
 python train.py --config path/to/config/file.yaml
 
-# for creating a video while evaluating a trained model
-python eval.py --config ../pretrained/configs/pretrained-boxing-gen.yaml make_video True   
-
 # for showing the liveplot while using a trained model
-python eval.py --config ../pretrained/configs/pretrained-boxing-gen.yaml liveplot True
+python eval.py --config configs/gen-boxing.yaml liveplot True
 ```
 
-There are other example config files inside `configs`-Folder for various experiments. They can be used to train new models.
 
 ## A little deeper explanation about the framework
 
 The main file to call in this framework is `src/train.py` for training, and `src/eval.py` for evaluation. It is called together with a path to a config file. The structure of the config files are defined in `src/xrl/xrl_config.py`. A config file configures the Atari-Game, the selected algorithms and methods for the modular blocks and other training parameters.
+
+plus focus files
 
 Parameters set in the config file can be overwritten by calling the parameter and its new value in the command. For example:
 
 ```
 python src/train.py --config path/to/config/file.yaml device "cpu"
 ```
-
-## Feature extractor
-
-To call the feature extractor on the game SpaceInvaders in interactive mode:
-`python3 run_color_extractor.py -i -g SpaceInvaders` 
-
-This call overwrites the "device"-parameter to "cpu"
-
-TODO: Checkpoint and logs
--- experiments/utils/xrl_start.py
+++ experiments/utils/xrl_start.py
@@ -1,10 +1,6 @@
-# main file for all rl algos
-
-import random
 import torch
 import matplotlib.pyplot as plt
 
-from torch.distributions import Categorical
 from torchinfo import summary
 from tqdm import tqdm
 from rtpt import RTPT
@@ -13,33 +9,10 @@ from algos import reinforce
 from algos import genetic_rl as genetic
 from scobi import Environment
 
-#import xrl.utils.plotter as xplt
-import utils.video_logger as vlogger
-import utils.utils as xutils
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning) 
-# agent class
-#from agent import Agent
-
 dev = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-
-# helper function to select action from loaded agent
-# has random probability parameter to test stability of agents
-# function to select action by given features
-# TODO: Remove like genetic algo
-def select_action(features, policy, random_tr = -1, n_actions=3):
-    sample = random.random()
-    if sample > random_tr:
-        # calculate probabilities of taking each action
-        f = torch.tensor(features).unsqueeze(0).float().to(dev)
-        probs = policy(f)
-        # sample an action from that set of probs
-        sampler = Categorical(probs)
-        action = sampler.sample()
-    else:
-        action = random.randint(0, n_actions - 1)
-    return action
 
 def re_select_action(*args, **kwargs):
     return reinforce.select_action(*args, **kwargs)[0]
@@ -114,46 +87,12 @@ def use_genetic(cfg, mode):
     if mode == "train":
         genetic.train(cfg)
     else:
-        agent = genetic.eval_load(cfg, agent)
+        model, gen_select_action = genetic.eval_load(cfg)
         if mode == "eval":
-            play_agent(agent=agent, cfg=cfg)
+            play_agent(cfg, model, gen_select_action)
         elif mode == "explain":
             pass
             # explain(agent=agent, cfg=cfg)
-
-
-# init agent function
-def init_agent(cfg):
-    focus_mode = cfg.focus_mode
-    print("Focus mode:", focus_mode)
-    # init correct raw features extractor
-    rfe = None
-    if cfg.raw_features_extractor == "atariari":
-        print("Raw Features Extractor:", "atariari")
-        rfe = get_labels
-    elif cfg.raw_features_extractor == "CE":
-        print("Raw Features Extractor:", "ColorExtractor")
-        game = cfg.env_name.replace("Deterministic", "").replace("-v4", "")
-        rfe = ColorExtractor(game=game, load=False)
-    # set correct mifs
-    # and set agent
-    if focus_mode in ["scobot", "iscobot"]:
-        dummy_agent = Agent(f1=rfe, f2=calc_preset_mifs)
-        env = env_manager.make(cfg, True)
-        #n_actions = env.action_space.n
-        actions = env._env.unwrapped.get_action_meanings()
-        gametype = xutils.get_gametype(env)
-        _, _ = env.reset(), 0
-        obs, _, _, _, info = env.step(1)
-        raw_features = dummy_agent.image_to_feature(obs, info, gametype)
-        focus = Focus(cfg, raw_features, actions)
-        return Agent(f1=rfe, f2=focus.get_feature_vector)
-    elif focus_mode == "iscobot-preset":
-        return Agent(f1=rfe, f2=calc_preset_mifs)
-    else:
-        print("Unknown mode, terminating...")
-        exit(1)
-    
 
 
 # main function
