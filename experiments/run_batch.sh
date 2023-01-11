99dad95d3337efa33b30b76ffea721655fd2561d
Sebastian Szt <szt.sebastian@googlemail.com>
2023-01-11 12:42:45 +0100

    requirements fix

--- experiments/run_batch.sh
+++ experiments/run_batch.sh
@@ -2,7 +2,7 @@
 echo "Starting Jobs.."
 for i in 1 2 3 4
 do
-    python train.py --config configs/scobi/re-pong-r_pruned.yaml seed $i > joblogs/re_pong_pruned_$i.log &
+    python train.py --config configs/re-pong-r.yaml seed $i > joblogs/re_pong_r_$i.log &
     sleep 2
 done
 wait
