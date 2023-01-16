d277a98c73db4fdb72adc5c3ffe29fc007ad8519
Sebastian Szt <szt.sebastian@googlemail.com>
2023-01-16 02:34:46 +0100

    major performance tuning

--- scobi/utils/game_object.py
+++ scobi/utils/game_object.py
@@ -2,25 +2,22 @@
 # containing current and past coordinates
 # and rgb values
 
-import numpy as np
 
 class GameObject():
     def __init__(self, name, rgb=[0,0,0], wh=[0,0]):
         self.name = name
         # contains current and old coords
-        self.coord = [0,0,0,0]
+        self.coord_now = [0, 0]
+        self.coord_past = [0, 0]
         self.rgb = rgb
         self.wh = wh
         self.category = "None"
     # set new coords
     def update_coords(self, x, y):
-        self.coord = np.roll(self.coord, 2)
-        self.coord[0] = x
-        self.coord[1] = y
+        self.coord_past = self.coord_now
+        self.coord_now[0] = x
+        self.coord_now[1] = y
 
     # returns 2 lists with current and past coords
     def get_coords(self):
-        return [self.coord[0], self.coord[1]], [self.coord[2], self.coord[3]]
-
-
-
+        return self.coord_now, self.coord_past
