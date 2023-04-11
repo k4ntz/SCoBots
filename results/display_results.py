86034fd22b4b02111b0d2b258042da0bb115770d
Quentin Delfosse <quentin.delfosse@cs.tu-darmstadt.de>
2023-04-11 21:42:52 +0200

    Added human normalization

--- results/display_results.py
+++ results/display_results.py
@@ -1,12 +1,21 @@
 import json
 import numpy as np
 import pandas as pd
+import sys
 
-results = json.load(open("results.json", "r"))
+
 from_rainbow = pd.read_csv('from_rainbow.tsv', sep='\t').set_index('Game')
 from_rainbow.index = from_rainbow.index.str.capitalize()
-rb_res = from_rainbow['Rainbow']
-# dqn_res = from_rainbow['DQN']
+
+from_ddqn = pd.read_csv('from_ddqn.tsv', sep='\t').set_index('Game')
+
+random = from_ddqn['Random']
+human = from_ddqn['Human']
+rainbow = from_rainbow['Rainbow']
+
+
+
+results = json.load(open("results.json", "r"))
 games = []
 rewards_games_base = []
 rewards_games_pruned = []
@@ -35,6 +44,11 @@ for game, gresults in results.items():
     games.append(game_name)
     rewards_base = np.array([val['reward'] for name, val in gresults.items() if not "pruned" in name])
     rewards_pruned = np.array([val['reward'] for name, val in gresults.items() if "pruned" in name])
+    if "-hn" in sys.argv:
+        h_score = human[game_name]
+        r_score = random[game_name]
+        rewards_base = 100*(rewards_base - r_score)/(h_score - r_score)
+        rewards_pruned = 100*(rewards_pruned - r_score)/(h_score - r_score)
     if len(rewards_base) > 0:
         rewards_games_base.append(format_text(rewards_base))
     else:
@@ -47,8 +61,17 @@ for game, gresults in results.items():
 df = pd.DataFrame([rewards_games_base, rewards_games_pruned], columns=games, index=methods).T
 df.index.name='Game'
 
-rb_res = rb_res[games]
-df = pd.concat([df, rb_res], axis=1)
+
+
+if "-hn" in sys.argv:
+    rainbow = rainbow[games]
+    random = random[games]
+    human = human[games]
+    rainbow = rainbow.map(lambda x: x.replace(",", "")).astype(float)
+    rainbow = round(100 * (rainbow - random)/(human - random), 1)
+    df = pd.concat([df, rainbow], axis=1).rename(columns={0: "Rainbow"})
+else:
+    df = pd.concat([df, rainbow[games], random[games], human[games]], axis=1)
 # dqn_res = dqn_res[games]
 # df = pd.concat([df, dqn_res], axis=1)
 print(df)
