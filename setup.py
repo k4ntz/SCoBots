278461b3a5c3068bf895296e911f25e43bfc0e8f
MaggiR <mark.rothermel@web.de>
2023-08-31 17:01:52 +0200

    Implement setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,22 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup()
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.readlines()
+
+
+setup(
+    name='scobi',
+    version='0.0.0',
+    author='',
+    author_email='',
+    packages=find_packages(),
+    include_package_data=True,
+    description='Successive Concepet Bottleneck Interface',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=requirements
+)
