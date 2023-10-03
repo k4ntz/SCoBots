Update reward shaping, fix minor bugs, add more properties to OCAGameObject, extend default normalization by more functions

scobi/preprocessing.py
@@ -7,11 +7,17 @@ NORMALIZATION_FNS = {
     "POSITION_HISTORY": lambda x: x / 164,  # any object coords
     "POSITION": lambda x: x / 164,  # any object coords
     "DISTANCE": lambda x: x / 164,  # any object coords
+    "LINEAR_TRAJECTORY": lambda x: x / 164,  # any object coords
+    "EUCLIDEAN_DISTANCE": lambda x: x / 164,  # any object coords
+    "CENTER": lambda x: x / 164,  # any object coords
     "DIR_VELOCITY": lambda x: np.tanh(x / 5),  # velocities (typically inside [-5, 5])
-    "ORIENTATION": lambda x: x / 8,  # player
+    "VELOCITY": lambda x: np.tanh(x / 5),  # velocities (typically inside [-5, 5])
+    "ORIENTATION": lambda x: x / 15,  # inside [0, 15]
     "WIDTH": lambda x: x / 64,  # oxygen bar
     "VALUE": lambda x: x / 3,  # lives
     "COUNT": lambda x: x / 6,  # collected divers
+    "RGB": lambda x: x / 255,  # color
+    "COLOR": lambda x: x / 255,  # color
 }
 
 