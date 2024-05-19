bfb73c41aa3ad50ec4a307ba94a9ddf5d288fb63
Sebastian Szt <szt.sebastian@googlemail.com>
2024-05-19 23:54:31 +0200

    integrated viper

--- baselines/extract_generate_samples.py
+++ baselines/extract_generate_samples.py
@@ -13,6 +13,8 @@ from stable_baselines3.common.utils import set_random_seed
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.callbacks import CheckpointCallback, EveryNTimesteps, BaseCallback, CallbackList, EvalCallback
 from stable_baselines3.common.env_util import make_atari_env, make_vec_env
+from sklearn.tree import DecisionTreeClassifier
+from joblib import dump, load
 from pathlib import Path
 from typing import Callable
 from rtpt import RTPT
@@ -23,6 +25,7 @@ from remix.rules.ruleset import Ruleset
 import torch
 import numpy as np
 import tensorflow as tf
+from viper import VIPER
 
 EVAL_ENV_SEED = 84
 
@@ -56,22 +59,34 @@ class RemixModel():
         return np.array(out), None
 
 
+class DTClassifierModel():
+    def __init__(self, model) -> None:
+        self.name = "DT Classifier Model"
+        self.model = model
+
+    def predict(self, obs, deterministic=True):
+        out = self.model.predict(obs)
+        return np.array(out), None
+
+
 def flist(l):
     return ["%.2f" % e for e in l]
 
 
-def eval_agent(model, env, episodes, obs_save_file=None):
+def eval_agent(model, env, episodes, obs_save_file=None, acts_save_file=None):
     current_episode = 0
     rewards = []
     steps = []
     current_rew = 0
     current_step = 0
-    out_array = []
+    obs_out_array = []
+    acts_out_array = []
     obs = env.reset()
     while True:
+        obs_out_array.append(obs[0]) #unvec
         action, _ = model.predict(obs, deterministic=True)
+        acts_out_array.append(action[0]) #unvec
         obs, reward, done, _ = env.step(action)
-        out_array.append(obs[0]) #unvec
         current_rew += reward
         current_step += 1
         if done:
@@ -88,8 +103,10 @@ def eval_agent(model, env, episodes, obs_save_file=None):
             print(f"rewards: {flist(rewards)} | mean: {np.mean(rewards):.2f} \nsteps: {flist(steps)} | mean: {np.mean(steps):.2f}")
             if obs_save_file:
                 obs_save_file.unlink(missing_ok=True)
-                np.save(obs_save_file, out_array)
-                print(">>> Observations saved!")
+                np.save(obs_save_file, obs_out_array)
+                acts_save_file.unlink(missing_ok=True)
+                np.save(acts_save_file, acts_out_array)
+                print(">>> Observations & Actions saved!")
             print("--------------------------------------------\n")
             break
 
@@ -97,7 +114,8 @@ def eval_agent(model, env, episodes, obs_save_file=None):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", type=str, required=True, help="checkpoint folder containing 'best_model.zip' and 'best_vecnormalize.pkl'")
-    parser.add_argument("-e", "--episodes", type=int, required=False, help="number of episodes to generate samples from")
+    parser.add_argument("-r", "--rule_extraction", type=str, required=True, choices=["remix", "dt", "viper"], default="remix", help="rule extraction to use.")
+    parser.add_argument("-e", "--episodes", type=int, required=False, help="number of episodes to evaluate agents samples on")
     parser.add_argument("-n", "--name", type=str, required=False, help="experiment name")
     opts = parser.parse_args()
     
@@ -107,6 +125,7 @@ def main():
     episodes = 5
     focus_dir = "focusfiles"
     expname = opts.name if opts.name else "experiment"
+    rule_extract = opts.rule_extraction
     checkpoint_name = opts.input #"Asterix_s0_re_pr"
     checkpoint_options = checkpoint_name.split("_")
     if len(checkpoint_options) == 3:
@@ -132,8 +151,8 @@ def main():
     vecnorm_path = Path("baselines_extract_input",  checkpoint_name, vecnorm_str)
     output_path = Path("baselines_extract_output", checkpoint_name + "-" + expname)
     output_path.mkdir(parents=True, exist_ok=True)
-    outfile = output_path / "obs.npy"
-        
+    obs_outfile = output_path / "obs.npy"
+    acts_outfile = output_path / "acts.npy"    
 
     env = Environment(env_str,
                       focus_dir=focus_dir,
@@ -142,62 +161,92 @@ def main():
 
 
     # Original SB3 Model Eval and Trainset Generation
-    model = PPO.load(model_path)
+    model = PPO.load(model_path, device="cuda:0")
     sb3_model_wrapped = SB3Model(model=model)
     dummy_vecenv = DummyVecEnv([lambda :  env])
     vec_env = VecNormalize.load(vecnorm_path, dummy_vecenv)
     vec_env.seed = EVAL_ENV_SEED
     vec_env.training = False
     vec_env.norm_reward = False
-    eval_agent(sb3_model_wrapped, vec_env, episodes=episodes, obs_save_file=outfile)
+    eval_agent(sb3_model_wrapped, vec_env, episodes=episodes, obs_save_file=obs_outfile, acts_save_file=acts_outfile)
 
 
-    # Translation to Keras and Model Eval 
-    fnames = env.get_vector_entry_descriptions()
-    actions = env.action_space_description
-    input_size = len(fnames)
-    output_size = len(actions)
-    pi_hidden_sizes = model.policy.net_arch["pi"]
-    torch_act_f_name = str(model.policy.activation_fn).split(".")[-1][:-2]
-    keras_act_f = torch_act_f_name.lower() #works for relu, didnt test others
+    if rule_extract == "remix":
+        # Translation to Keras and Model Eval 
+        fnames = env.get_vector_entry_descriptions()
+        actions = env.action_space_description
+        input_size = len(fnames)
+        output_size = len(actions)
+        pi_hidden_sizes = model.policy.net_arch["pi"]
+        torch_act_f_name = str(model.policy.activation_fn).split(".")[-1][:-2]
+        keras_act_f = torch_act_f_name.lower() #works for relu, didnt test others
 
-    #hardcoded to 2 hidden layers for now. should work for arbitrary sizes
-    pi_weight_keys = ['mlp_extractor.policy_net.0.weight', 
-               'mlp_extractor.policy_net.0.bias',
-               'mlp_extractor.policy_net.2.weight',
-               'mlp_extractor.policy_net.2.bias',
-               'action_net.weight',
-               'action_net.bias']
+        #hardcoded to 2 hidden layers for now. should work for arbitrary sizes
+        pi_weight_keys = ['mlp_extractor.policy_net.0.weight', 
+                'mlp_extractor.policy_net.0.bias',
+                'mlp_extractor.policy_net.2.weight',
+                'mlp_extractor.policy_net.2.bias',
+                'action_net.weight',
+                'action_net.bias']
 
-    weights = model.policy.state_dict()
-    keras_model = tf.keras.Sequential()
-    keras_model.add(tf.keras.Input(shape=(input_size,)))
-    keras_model.add(tf.keras.layers.Activation("linear"))
-    keras_model.add(tf.keras.layers.Dense(pi_hidden_sizes[0], activation=keras_act_f))
-    keras_model.add(tf.keras.layers.Dense(pi_hidden_sizes[1], activation=keras_act_f))
-    keras_model.add(tf.keras.layers.Dense(output_size))
-    keras_model.add(tf.keras.layers.Softmax())
-    keras_model.set_weights([weights[key].cpu().numpy().T for key in pi_weight_keys])
-    #keras_model.summary()
+        weights = model.policy.state_dict()
+        keras_model = tf.keras.Sequential()
+        keras_model.add(tf.keras.Input(shape=(input_size,)))
+        keras_model.add(tf.keras.layers.Activation("linear"))
+        keras_model.add(tf.keras.layers.Dense(pi_hidden_sizes[0], activation=keras_act_f))
+        keras_model.add(tf.keras.layers.Dense(pi_hidden_sizes[1], activation=keras_act_f))
+        keras_model.add(tf.keras.layers.Dense(output_size))
+        keras_model.add(tf.keras.layers.Softmax())
+        keras_model.set_weights([weights[key].cpu().numpy().T for key in pi_weight_keys])
+        #keras_model.summary()
 
-    keras_model_wrapped = KerasModel(keras_model)
-    eval_agent(keras_model_wrapped, vec_env, episodes=episodes)
+        keras_model_wrapped = KerasModel(keras_model)
+        eval_agent(keras_model_wrapped, vec_env, episodes=episodes)
+        
+
+        # Rule extraction via ECLAIRE
+        ruleset_fname = checkpoint_name + ".rules"
+        trainset = np.load(obs_outfile)
+        input = tf.convert_to_tensor(trainset.astype(np.float32))
+        clean_fnames = [s.replace(' ', '') for s in fnames] # need to remove whitespaces in names otherwise remix gets mad
+        ruleset = eclaire.extract_rules(keras_model, input, feature_names=clean_fnames, output_class_names=actions)
+        ruleset.to_file(output_path / ruleset_fname)
+        print(f"Ruleset saved as '%s' !" % ruleset_fname)
+
+        # Eval ruleset
+        ruleset = Ruleset().from_file(output_path / ruleset_fname)
+        ruleset_model_wrapped = RemixModel(ruleset)
+        eval_agent(ruleset_model_wrapped, vec_env, episodes=episodes)
+        print("Done!")
     
-
-    # Rule extraction via ECLAIRE
-    ruleset_fname = checkpoint_name + ".rules"
-    trainset = np.load(outfile)
-    input = tf.convert_to_tensor(trainset.astype(np.float32))
-    clean_fnames = [s.replace(' ', '') for s in fnames] # need to remove whitespaces in names otherwise remix gets mad
-    ruleset = eclaire.extract_rules(keras_model, input, feature_names=clean_fnames, output_class_names=actions)
-    ruleset.to_file(output_path / ruleset_fname)
-    print(f"Ruleset saved as '%s' !" % ruleset_fname)
-
-    # Eval ruleset
-    ruleset = Ruleset().from_file(output_path / ruleset_fname)
-    ruleset_model_wrapped = RemixModel(ruleset)
-    eval_agent(ruleset_model_wrapped, vec_env, episodes=episodes)
-    print("Done!")
-
+    elif rule_extract == "dt":
+        # Rule extraction via sklearn DTClassifier
+        MAX_DEPTH = 6
+        dt_fname = checkpoint_name + ".dt"
+        train_observations = np.load(obs_outfile)
+        train_actions = np.load(acts_outfile)
+        dtree = DecisionTreeClassifier(max_depth=MAX_DEPTH)
+        dtree.fit(train_observations, train_actions)
+        dtclassifier_wrapped = DTClassifierModel(dtree)
+        eval_agent(dtclassifier_wrapped, vec_env, episodes=episodes)
+        dump(dtree, output_path / dt_fname)
+        print("DT leaves: %s | DT depth %s" % (dtree.get_n_leaves(), dtree.get_depth()))
+        print("Done!")
+    elif rule_extract == "viper":
+        MAX_DEPTH = 5
+        train_observations = np.load(obs_outfile)
+        train_actions = np.load(acts_outfile)
+        clf = DecisionTreeClassifier(max_depth=MAX_DEPTH)
+        vip = VIPER(model, clf, vec_env)
+        vip.imitate(nb_iter=10)
+        vip.save_best_tree(output_path)
+        best_viper = sorted(output_path.glob("*_best.viper"))
+        if not best_viper:
+            print("error")
+            exit()
+        dtree = load(best_viper[0]) #only one should exist
+        viper_wrapped = DTClassifierModel(dtree)
+        eval_agent(viper_wrapped, vec_env, episodes=episodes)
+        print("Done!")
 if __name__ == '__main__':
     main()
---
+++ baselines/viper.py
@@ -0,0 +1,155 @@
+from stable_baselines3 import PPO
+from sklearn.tree import DecisionTreeClassifier
+import numpy as np
+from tqdm import tqdm
+from joblib import dump
+from statistics import mean
+from copy import deepcopy
+from operator import itemgetter
+from gymnasium import Env
+from pathlib import Path
+import os
+import torch
+import time
+
+# from viperoc repository
+cuda = torch.device("cuda")
+class LogProbQ:
+    def __init__(self, stochastic_pol: PPO, env: Env):
+        self.pol = stochastic_pol
+        self.env = env
+    
+    def q(self, s):
+        s = torch.Tensor(s).to(cuda)
+        s_repeat = s.repeat(self.env.action_space.n,1)
+        with torch.no_grad():
+            _, s_a_log_probs, _ = self.pol.policy.evaluate_actions(s_repeat, torch.arange(self.env.action_space.n).reshape(-1, 1).to(cuda))
+        return s_a_log_probs
+    
+    def get_disagreement_cost(self, s):
+        log_prob = self.q(s)
+        return log_prob.mean() - log_prob.min()
+
+
+class DecisionTreeExtractor: #Dagger
+    def __init__(self, model: PPO, dtpolicy: DecisionTreeClassifier, env: Env, data_per_iter: int=20_000):
+        self.model = model
+        self.env = env # is vectorized
+        self.data_per_iter = data_per_iter
+        self.dt = dtpolicy
+
+    def collect_data(self):
+        S, A = [], []
+        s = self.env.reset()
+        for i in tqdm(range(self.data_per_iter)):
+            action = self.model.predict(s, deterministic=True)[0]
+            S.append(s[0]) #unvec
+            A.append(action[0]) #unvec
+            s, _, done, _ = self.env.step(action)
+            if done:
+                s = self.env.reset()
+        return np.array(S), np.array(A)
+
+    def collect_data_dt(self,):
+        S = []
+        episodes = []
+        ep_reward = 0
+        s = self.env.reset()
+        for i in range(self.data_per_iter):
+            action = self.dt.predict(s.reshape(1, -1))
+            S.append(s[0]) #unvec
+            s, r, done, infos = self.env.step(action)
+            ep_reward += r
+            if done:
+                s = self.env.reset()
+                episodes.append(ep_reward)
+                ep_reward = 0
+        if len(episodes) < 1:
+            episodes.append(ep_reward)
+        return S, np.mean(episodes)
+
+    def fit_DT(self, S, A):
+        ## sampling
+        self.dt.fit(S, A)
+        acc = self.dt.score(S, A)
+        return acc
+
+    def imitate(self, nb_iter: int):
+        start_time = time.time()
+        self.list_acc, self.list_eval, self.list_dt, self.times = [], [], [], []
+        DS, DA = self.collect_data()
+        acc_dt = self.fit_DT(DS, DA)
+        S_dt, eval_dt = self.collect_data_dt()
+        self.times.append(time.time()-start_time)
+
+        print("Accuracy: {} - Evaluation: {}".format(acc_dt, eval_dt))
+        self.list_dt.append(deepcopy(self.dt))
+        self.list_acc.append(acc_dt)
+        self.list_eval.append(eval_dt)
+        DS = np.concatenate((DS, S_dt))
+        DA = np.concatenate((DA, self.model.predict(S_dt)[0]))
+        
+        for _ in range(nb_iter - 1):
+            acc_dt = self.fit_DT(DS, DA)
+            S_dt, eval_dt = self.collect_data_dt()
+            self.times.append(time.time()-start_time)
+
+            print("Accuracy: {} - Evaluation: {}".format(acc_dt, eval_dt))
+            self.list_dt.append(deepcopy(self.dt))
+            self.list_acc.append(acc_dt)
+            self.list_eval.append(eval_dt)
+            DS = np.concatenate((DS, S_dt))
+            DA = np.concatenate((DA, self.model.predict(S_dt)[0]))
+
+    def save_best_tree(self, out_path):
+        trees_path = out_path / Path("viper_trees")
+        trees_path.mkdir(parents=True, exist_ok=True)
+
+        for j, tree in enumerate(self.list_dt):
+            fpath = "Tree-%s_%s.viper" % (j, self.list_eval[j])
+            dump(tree, trees_path / fpath)
+
+        index, element = max(enumerate(self.list_eval), key=itemgetter(1))
+        self.best_dt = self.list_dt[index]
+        best_fpath = "Tree-"+str(element) + "_best.viper"
+        dump(self.best_dt, out_path / best_fpath)
+    
+
+class VIPER(DecisionTreeExtractor):
+    def __init__(self, model: PPO, dtpolicy: DecisionTreeClassifier, env: Env, data_per_iter: int=20_000):
+        super().__init__(model, dtpolicy, env, data_per_iter)
+        self.Q = LogProbQ(self.model, self.env)
+
+    def fit_DT(self, S, A, weights):
+        self.dt.fit(S, A, weights)
+        acc = self.dt.score(S, A, weights)
+        return acc
+    
+    def imitate(self, nb_iter: int):
+        start_time = time.time()
+        self.list_acc, self.list_eval, self.list_dt, self.times =[], [], [], []
+        DS, DA = self.collect_data()
+        weights = [self.Q.get_disagreement_cost(s).item() for s in DS] # could be sped up
+
+        acc_dt = self.fit_DT(DS, DA, weights)
+        S_dt, eval_dt = self.collect_data_dt()
+        self.times.append(time.time()-start_time)
+        print("Accuracy: {} - Evaluation: {}".format(acc_dt, eval_dt))
+        self.list_dt.append(deepcopy(self.dt))
+        self.list_acc.append(acc_dt)
+        self.list_eval.append(eval_dt)
+        DS = np.concatenate((DS, S_dt))
+        DA = np.concatenate((DA, self.model.predict(S_dt)[0]))
+        weights += [self.Q.get_disagreement_cost(s).item() for s in S_dt]
+        
+        for _ in range(nb_iter - 1):
+            acc_dt = self.fit_DT(DS, DA, weights)
+            S_dt, eval_dt = self.collect_data_dt()
+            self.times.append(time.time()-start_time)
+            print("Accuracy: {} - Evaluation: {}".format(acc_dt, eval_dt))
+            self.list_dt.append(deepcopy(self.dt))
+            self.list_acc.append(acc_dt)
+            self.list_eval.append(eval_dt)
+            DS = np.concatenate((DS, S_dt))
+            DA = np.concatenate((DA, self.model.predict(S_dt)[0]))
+            weights += [self.Q.get_disagreement_cost(s).item() for s in S_dt]
